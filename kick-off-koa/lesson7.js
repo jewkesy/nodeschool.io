420129f4787dd4c0242d89a4eb70c26af6351c69
Daryl Jewkes <github@daryljewkes.com>
2014-08-28 09:57:38 +0100

    completed lesson 7

---
+++ kick-off-koa/lesson7.js
@@ -0,0 +1,24 @@
+var koa = require('koa');
+
+var app = koa();
+
+app.use(errorHandler());
+
+app.use(function* () {
+  if (this.path === '/error') throw new Error('ooops');
+  this.body = 'OK';
+});
+
+function errorHandler() {
+  return function* (next) {
+    // try catch all downstream errors here
+    try {
+      yield next;
+    } catch (err) {
+      this.status = 500;
+      this.body = 'internal server error';
+    }
+  };
+}
+
+app.listen(process.argv[2]);
