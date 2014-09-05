761479391d562e375cf28fe1606c7471f224090d
Daryl Jewkes <github@daryljewkes.com>
2014-09-05 12:30:25 +0100

    finished lesson 1

--- async-you/lesson1.js
+++ async-you/lesson1.js
@@ -2,7 +2,6 @@ var fs = require('fs')
   , http = require('http')
   , async = require('async');
 
-
 async.waterfall([
     function(callback){
       fs.readFile(process.argv[2], function(err, data) {
@@ -35,31 +34,3 @@ async.waterfall([
    // result now equals 'done'
    console.log(result);
 });
-
-
-// async.waterfall([
-//   function(done){
-//     fs.readFile(process.argv[2], function(err, data){
-//       if (err) return done(err);
-//       done(null, data)
-//     });
-//   },
-//
-//   function(data, done){
-//     var body = '';
-//     http.get(data.toString().trimRight(), function(res){
-//       res.on('data', function(chunk){
-//         body += chunk.toString();
-//       });
-//
-//       res.on('end', function(chunk){
-//         done(null, body);
-//       });
-//     }).on('error', function(e){
-//       done(e);
-//     });
-//   }
-// ], function(err, result){
-//   if (err) return console.error(err);
-//   console.log(result);
-// });
